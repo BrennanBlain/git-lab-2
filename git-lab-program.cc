f16dfdf0e2e9556e04b639af7d24047298c7788d
BrennanBlain <bb828119@ohio.edu>
2023-01-24 21:09:21 -0500

    Created new file

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Brennan Blain>
+ *        Date: <1/24/2023>
+ * Description: Add Hello git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
